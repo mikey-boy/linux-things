400497189b561e30a88c2eb3f13fe7126599dece
mikey-boy <michael.argany@protonmail.com>
2024-02-13 13:35:43 -0500

    Added zettelkasten script and updated markdown colorscheme through autocmd

--- dotfiles/nvim/lua/config/autocmds.lua
+++ dotfiles/nvim/lua/config/autocmds.lua
@@ -7,5 +7,11 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   callback = function()
     vim.cmd("NoNeckPain")
     vim.cmd("set nospell")
+    vim.cmd("hi @markup.heading.1.markdown guifg=#458588 gui=bold")
+    vim.cmd("hi @markup.heading.2.markdown guifg=#d79921 gui=bold")
+    vim.cmd("hi @markup.heading.3.markdown guifg=#98971a gui=bold")
+    vim.cmd("hi @markup.heading.1.marker.markdown guifg=#458588 gui=bold")
+    vim.cmd("hi @markup.heading.2.marker.markdown guifg=#d79921 gui=bold")
+    vim.cmd("hi @markup.heading.3.marker.markdown guifg=#98971a gui=bold")
   end,
 })
